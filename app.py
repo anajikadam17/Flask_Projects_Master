ce652998b7507e7f6c6cae9f431e489c96c7c94e
anajikadam17 <anajikadam17@gmail.com>
2020-11-04 18:49:43 +0530

    commit

--- app.py
+++ app.py
@@ -13,8 +13,6 @@ warnings.filterwarnings("ignore", category=FutureWarning)
 warnings.filterwarnings("ignore", category=UserWarning)
 
 from model_1 import PredictSalary
-from model_2 import SpamPrediction
-from model_3 import FlightFarePredict
 
 app = Flask(__name__)
 
@@ -41,43 +39,6 @@ def predict1():
 def index():
     return render_template("index.html")
 
-@app.route("/task2")
-def task2():
-    return render_template("task2.html")
-@app.route('/predict2',methods=['POST'])
-def predict2():
-    """
-        Predict text spam or ham
-    """
-    if request.method == 'POST':
-        message = request.form['message']
-        message = [message]
-        SpamPredictionObj = SpamPrediction()
-        my_prediction = SpamPredictionObj.predictSpam(message)
-    return render_template('task2.html',prediction = my_prediction)
-
-@app.route("/task3")
-def task3():
-    return render_template("task3.html")
-@app.route('/predict3', methods = ["GET", "POST"])
-@cross_origin()
-def predict3():
-    if request.method == "POST":
-        date_dep = request.form["Dep_Time"]
-        date_arr = request.form["Arrival_Time"]
-        Total_stops = int(request.form["stops"])
-        airline = request.form['airline']
-        Source = request.form["Source"]
-        Dest = request.form["Destination"]
-        FlightFarePredictObj = FlightFarePredict()
-        my_prediction = FlightFarePredictObj.predictFlightFare(date_dep, 
-                                            date_arr, Total_stops, airline,
-                                            Source, Dest)
-        output=round(my_prediction[0],2)
-        return render_template('task3.html', 
-                           prediction_text="Your Flight price is Rs. {}"
-                           .format(output))
-    return render_template("task3.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
